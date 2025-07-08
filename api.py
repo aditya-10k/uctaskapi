006b13d8f495af6174d391ee75d394caae97ecce
aditya-10k <katheaditya10@gmail.com>
2025-07-08 17:10:18 +0530

    2nd commit

--- api.py
+++ api.py
@@ -1,11 +1,16 @@
+import os
 from flask import Flask, jsonify
 import pandas as pd
 import random
 
 app = Flask(__name__)
 
-# Load CSV into memory once
+# Load CSV once
 df = pd.read_csv("moviedata.csv")
+
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify({"message": "🎬 Welcome to the Movie API! Use /random to get a random movie."})
 
 @app.route("/random", methods=["GET"])
 def random_movie():
@@ -13,4 +18,5 @@ def random_movie():
     return jsonify(movie)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # default to 5000 locally
+    app.run(host="0.0.0.0", port=port)
